6013c3ea5c286f187c34629f0d89aa800819f848
Hari Prasad <haririo321@gmail.com>
2024-10-08 10:34:16 +0530

    gpa: small dependency changes

--- gym-project-main/src/App.tsx
+++ gym-project-main/src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import TimeSlotBooking from './components/TimeSlotBooking';
-import HomePage from './pages/home';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import TimeSlotBooking from "./components/TimeSlotBooking";
+import HomePage from "./pages/home";
 
 const App: React.FC = () => {
   return (
