7fb4f7a86d65d16698bead31fe8f36e9fa49da46
Hari Prasad <haririo321@gmail.com>
2024-10-09 11:49:04 +0530

    gpa: created a types.d.ts for the models

---
+++ gym-project-main/types.d.ts
@@ -0,0 +1,11 @@
+interface Gym {
+    gym_name : string; 
+    created_on : Date;
+    opening_time: string;
+    closing_time: string;
+    manager_name : string;
+    manager_email: string;
+    is_open : boolean;
+    gym_address : string;
+    gym_city : string;
+}
