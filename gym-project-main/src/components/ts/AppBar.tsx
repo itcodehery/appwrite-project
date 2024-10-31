2d894d035e34d6ab616042fb6c096a9b908e7ee6
Hari Prasad <haririo321@gmail.com>
2024-10-31 17:47:10 +0530

    changed appbar

--- gym-project-main/src/components/ts/AppBar.tsx
+++ gym-project-main/src/components/ts/AppBar.tsx
@@ -19,85 +19,6 @@ const AppBar = ({ is_for_user }: AppBarType) => {
     });
   };
   if (!is_for_user) {
-    // return (
-    //   <header className="navbar">
-    //     <div className="logo">
-    //       {/* Typewriter effect for the logo */}
-    //       <motion.h1
-    //         initial={{ opacity: 0, y: -50 }}
-    //         animate={{ opacity: 1, y: 0 }}
-    //         transition={{ duration: 1 }}
-    //       >
-    //         <Typewriter
-    //           words={["SpotFit"]}
-    //           loop={1}
-    //           cursor
-    //           cursorStyle=""
-    //           typeSpeed={100}
-    //           deleteSpeed={50}
-    //           delaySpeed={1000}
-    //         />
-    //       </motion.h1>
-    //     </div>
-    //     <nav className="nav-links">
-    //       {/* Animated navigation links */}
-    //       <motion.div
-    //         color="red"
-    //         initial={{ opacity: 0, y: -50 }}
-    //         animate={{ opacity: 1, y: 0 }}
-    //         transition={{ duration: 1, delay: 0.3 }}
-    //       >
-    //         <Link to="/home">Home</Link>
-    //       </motion.div>
-    //       <motion.div
-    //         initial={{ opacity: 0, y: -50 }}
-    //         animate={{ opacity: 1, y: 0 }}
-    //         transition={{ duration: 1, delay: 0.5 }}
-    //       >
-    //         <Link to="/gyms">Gyms</Link>
-    //       </motion.div>
-    //       <motion.div
-    //         initial={{ opacity: 0, y: -50 }}
-    //         animate={{ opacity: 1, y: 0 }}
-    //         transition={{ duration: 1, delay: 0.7 }}
-    //       >
-    //         <Link to="/admindash">T: Admin</Link>
-    //       </motion.div>
-    //       <motion.div
-    //         initial={{ opacity: 0, y: -50 }}
-    //         animate={{ opacity: 1, y: 0 }}
-    //         transition={{ duration: 1, delay: 0.9 }}
-    //       >
-    //         <Link to="/membership">Membership</Link>
-    //       </motion.div>
-    //       <motion.div
-    //         initial={{ opacity: 0, y: -50 }}
-    //         animate={{ opacity: 1, y: 0 }}
-    //         transition={{ duration: 1, delay: 1.1 }}
-    //       >
-    //         <Link to="/userhome">T: UserHome</Link>
-    //       </motion.div>
-    //     </nav>
-    //     <motion.div
-    //       className="nav-actions"
-    //       initial={{ scale: 0 }}
-    //       animate={{ scale: 1 }}
-    //       transition={{ duration: 0.5, delay: 1.3 }}
-    //     >
-    //       <Link to="/cart" className="cart-icon">
-    //         <span role="img" aria-label="cart">
-    //           🛒
-    //         </span>
-    //       </Link>
-    //       <Link to="/login" className="login-btn">
-    //         Log In
-    //       </Link>
-    //       <Link to="/signup" className="signup-btn">
-    //         Sign Up
-    //       </Link>
-    //     </motion.div>
-    //   </header>
-    // );
     return (
       <header className="navbar">
         <div>
@@ -109,9 +30,6 @@ const AppBar = ({ is_for_user }: AppBarType) => {
           </Link>
           <Link to="/trainers" className="action">
             Trainers
-          </Link>
-          <Link to="/membership" className="action">
-            Membership
           </Link>
         </div>
         <div className="action-row">
@@ -151,9 +69,6 @@ const AppBar = ({ is_for_user }: AppBarType) => {
           </Link>
           <Link to="/trainers" className="action">
             Trainers
-          </Link>
-          <Link to="/membership" className="action">
-            Membership
           </Link>
         </div>
         <button className="login-but" onClick={logItOut}>
